cf90b5d4ab023ea6d0b003901bd9b86156119fc1
ayomidepayment01-cloud <ayomidepayment01@gmail.com>
2026-06-24 20:40:01 +0100

    Fix: Prevent make_admin crash when user ayomide does not exist yet

--- legacy_core/management/commands/make_admin.py
+++ legacy_core/management/commands/make_admin.py
@@ -2,9 +2,14 @@ from django.core.management.base import BaseCommand
 from legacy_core.models import CustomUser
 
 class Command(BaseCommand):
+    help = 'Elevates user ayomide to superuser'
+
     def handle(self, *args, **options):
-        user = CustomUser.objects.get(username='ayomide')
-        user.is_staff = True
-        user.is_superuser = True
-        user.save()
-        self.stdout.write("ayomide is now admin!")
+        try:
+            user = CustomUser.objects.get(username='ayomide')
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+            self.stdout.write(self.style.SUCCESS("ayomide is now admin!"))
+        except CustomUser.DoesNotExist:
+            self.stdout.write(self.style.WARNING("User 'ayomide' does not exist in the database. Please register first on the website, then run this command again."))
