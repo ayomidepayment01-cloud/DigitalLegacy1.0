f1d18dce787dfa84d68c6b6dfbf8e668a166671f
ayomidepayment01-cloud <ayomidepayment01@gmail.com>
2026-06-29 13:49:37 +0100

    Prevent settings.py from crashing if Render variables are missing

--- legacy_backend/settings.py
+++ legacy_backend/settings.py
@@ -101,8 +101,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = 'no-reply@digitallegacy.com' # Use your verified domain/email
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
